1e945651a2286a930ad1e374dacf66e54adfa7a7
186510kristoffer <krissalbrigtsen@gmail.com>
2025-02-17 13:45:20 +0100

    fikset main

--- src/del1/Oppgave6Main.java
+++ src/del1/Oppgave6Main.java
@@ -1,21 +1,29 @@
 package del1;
 
+import del1.ParentesSjekker;
+
 public class Oppgave6Main {
     public static void main(String[] args) {
         ParentesSjekker sjekker = new ParentesSjekker();
 
-        String[] tester = { "{ [ ( ) ] }", // Korrekt
-                "{ [ ( ) }", // Ikke korrekt
-                "[ ( ) ] }", // Ikke korrekt
+        String[] tester = { 
+                "{ [ ( ) ] }", // Korrekt
+                "{ [ ( ) }",   // Ikke korrekt
+                "[ ( ) ] }",   // Ikke korrekt
                 "{ [ ( ] ) }", // Ikke korrekt
-                "class HelloWorld { public static void main(String[] args) { System.out.println("Hello World!"); } }" // Korrekt
+                "class HelloWorld { public static void main(String[] args) { System.out.println(\"Hello World!\"); } }" // Korrekt
         };
 
         for (String test : tester) {
-            System.out.println("Tester: " + test);
-            System.out.println("Resultat: " + sjekker.sjekkParenteser(test));
+            try {
+                System.out.println("Tester: " + test);
+                System.out.println("Resultat: " + sjekker.sjekkParenteser(test));
+            } catch (Exception e) {
+                System.out.println("Feil oppstod: " + e.getMessage());
+            }
             System.out.println("---------------------");
         }
     }
 }
+
 
