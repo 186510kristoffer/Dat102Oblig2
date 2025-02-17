14422ff932e9bb51f239d1df8524022161d69f9d
EndreNoren <endrenoren@gmail.com>
2025-02-17 13:31:21 +0100

    update

---
+++ src/del1/ParantesSjekkerTest.java
@@ -0,0 +1,31 @@
+package del1;
+
+	import org.junit.jupiter.api.Test;
+
+	class ParentesSjekkerTest {
+
+	    private final ParentesSjekker sjekker = new ParentesSjekker();
+
+	    @Test
+	    void testKorrektParenteser() {
+	        assertTrue(sjekker.sjekkParenteser("{ [ ( ) ] }"), "Skal være korrekt");
+	        assertTrue(sjekker.sjekkParenteser("class HelloWorld { public static void main(String[] args) { System.out.println(\"Hello World!\"); } }"), "Skal være korrekt");
+	    }
+
+	    @Test
+	    void testManglerSluttParentes() {
+	        assertFalse(sjekker.sjekkParenteser("{ [ ( ) }"), "Mangler sluttparentes");
+	    }
+
+	    @Test
+	    void testManglerStartParentes() {
+	        assertFalse(sjekker.sjekkParenteser("[ ( ) ] }"), "Mangler startparentes");
+	    }
+
+	    @Test
+	    void testFeilRekkefølge() {
+	        assertFalse(sjekker.sjekkParenteser("{ [ ( ] ) }"), "Feil rekkefølge på parenteser");
+	    }
+	}
+
+
