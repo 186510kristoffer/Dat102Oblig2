cc069c46e28378227d65186ca8dc15dab0e443f9
EndreNoren <endrenoren@gmail.com>
2025-02-18 12:41:15 +0100

    update

--- src/del2/Sortering.java
+++ src/del2/Sortering.java
@@ -1,10 +1,24 @@
 package del2;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Sortering {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int storrelse = 500; 
+		int[] tallTabell = new int[storrelse]; 
 
+		Random rand = new Random(); 
+
+	
+		for (int i = 0; i < tallTabell.length; i++) {
+			tallTabell[i] = rand.nextInt(1000) + 1;
+		}
+		System.out.print(Arrays.toString(tallTabell));
+		
+	
+		}
 	}
 
-}
+
